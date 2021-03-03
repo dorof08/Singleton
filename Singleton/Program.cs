3d84236629740e75b0cf9013077a3219ff156313
Ivan <ivan.pali4@gmail.com>
2021-03-04 01:56:01 +0300

    Initial commit

---
+++ Singleton/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Singleton
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var singleton1 = SingletonReadWriter.Instance;
+			var singleton2 = SingletonReadWriter.Instance;
+
+			singleton1.WtiteText("s1");
+			singleton2.WtiteText("s2");
+
+			singleton1.Save();
+			singleton2.Save();
+		}
+	}
+}
---
+++ Singleton/SingletonReadWriter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace Singleton
+{
+	sealed class SingletonReadWriter
+	{
+		private static readonly Lazy<SingletonReadWriter> instance = new Lazy<SingletonReadWriter>(() => new SingletonReadWriter());
+
+		public static SingletonReadWriter Instance { get { return instance.Value; } }
+
+		public string FilePath { get; }
+		public string Text { get; private set; }
+
+		private SingletonReadWriter()
+		{
+			FilePath = "text.txt";
+			ReadFromTxt();
+		}
+
+		public void WtiteText(string text)
+		{
+			Text += text;
+		}
+
+		public void Save()
+		{
+			using (var writer = new StreamWriter(FilePath))
+				writer.WriteLine(Text);
+		}
+
+		private void ReadFromTxt()
+		{
+			if (File.Exists(FilePath))
+			{
+				using (var reader = new StreamReader(FilePath))
+					Text = reader.ReadToEnd();
+			}
+			else
+			{
+				Text = "";
+			}
+		}
+	}
+}
